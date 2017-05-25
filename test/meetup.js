b843246f0d0672e7d137e309fb0c141abe6bcc09
Miguel Mota <miguelmota2@gmail.com>
2017-05-25 11:01:55 -0700

    create landing page

--- test/meetup.js
+++ test/meetup.js
@@ -1,20 +1,24 @@
-var Meetup = artifacts.require("./Meetup.sol")
+var Meetup = artifacts.require('./Meetup.sol')
 
 contract('Meetup', function(accounts) {
-  it("should create a meetup", async function() {
+  it('should create a meetup', async function() {
     //console.log(accounts)
     var organizer = accounts[0]
 
     try {
       var instance = await Meetup.deployed()
+
+      // create meetup
       var result = await instance.createMeetup('Hello', 'World')
       assert.ok(result.tx)
 
+      // get all meetup hashes for organizer
       var meetupHashes = await instance.getMeetupHashes.call(organizer)
       console.log(meetupHashes)
       assert.equal(meetupHashes.length, 1)
       assert.equal(typeof meetupHashes[0], 'string')
 
+      // get meetup details for meetup hash
       var meetup = await instance.getMeetup.call(meetupHashes[0])
       console.log(meetup)
       var [title, description] = meetup
