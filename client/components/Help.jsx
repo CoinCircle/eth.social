b69b16619e1107cabb3951bd95b1f1a53c97765f
Miguel Mota <miguelmota2@gmail.com>
2017-06-10 19:27:25 -0700

    update help

--- client/components/Help.jsx
+++ client/components/Help.jsx
@@ -18,14 +18,21 @@ class Help extends React.Component {
         <h3>Set Ethereum Provider</h3>
         <p>Currently eth.social is in a testing and development phase so it's not available in the mainnet yet.</p>
 
-        {//<p>Set ethereum provider to "<a href="https://www.rinkeby.io/" target="_blank">Rinkeyby Test Network</a>".</p>}
-        {//<img src="https://gateway.ipfs.io/ipfs/QmNoEWq11GLstCJEffpG4ho7jEBJyyKQgzNsD8z5GTq5f5" width="300" />}
+        {
+        //<p>Set ethereum provider to "<a href="https://www.rinkeby.io/" target="_blank">Rinkeyby Test Network</a>".</p>
+        }
+        {
+        //<img src="https://gateway.ipfs.io/ipfs/QmNoEWq11GLstCJEffpG4ho7jEBJyyKQgzNsD8z5GTq5f5" width="300" />
+        //
+        }
 
         <p>Set ethereum provider to "<a href="https://kovan.etherscan.io/" target="_blank">Kovan Test Network</a>".</p>
         <img src="https://gateway.ipfs.io/ipfs/QmRVRjWnWtbUKMAfZ3W8oruaXTeGUiszeA8cH5YMzNHzUV" width="300" />
 
         <h3>Deposit ether</h3>
-        {//<p>Use the <a href="https://www.rinkeby.io/" target="_blank">rinkeyby faucet</a> if you don't already have ether in your account.</p>}
+        {
+        //<p>Use the <a href="https://www.rinkeby.io/" target="_blank">rinkeyby faucet</a> if you don't already have ether in your account.</p>
+        }
         <p>Use the <a href="https://gitter.im/kovan-testnet/faucet" target="_blank">Kovan faucet</a> if you don't already have ether in your account.</p>
 
         </div>
